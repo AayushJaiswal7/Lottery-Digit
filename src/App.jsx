2bc4c44c342ef46e472a9b4fe330b50f358e281f
Aayush Kumar <aayushkr799@gmail.com>
2026-05-26 00:48:45 +0530

    feat: implement lottery game logic with ticket generation and win condition validation components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Lottery from "./Lottery.jsx";
+
+
+export default function App() {
+
+   let winCondition = (ticket) => {
+      return ticket.every((num) => num === ticket[0]);
+   };
+
+   return (
+      <>
+         <Lottery n={3} winningSum={winCondition} />
+      </>
+   );
+
+}
